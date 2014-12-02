9b60c071e9c65a4682b84649d986f2ad4dd32ae0
Navi <Navi@navi.home>
2014-12-02 23:39:31 +1100

    Initial Commit

---
+++ HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Navi on 14/12/2.
+//  Copyright (c) 2014年 Navi@RMIT. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
